828c4888623580a21ee49d2929e35f52e8ef5707
scailancrei <juanantonio.dev@hotmail.com>
2026-05-31 18:03:03 +0200

    enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,19 +1,19 @@
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+        dbg!(&self);
+    }
+}
+
 fn main() {
-    #[derive(Debug)]
-    enum Message {
-        Quit,
-        Move { x: i32, y: i32 },
-        Write(String),
-        ChangeColor(i32, i32, i32),
-    }
-
-    impl Message {
-        fn call(&self) {
-            // method body would be defined here
-            dbg!(&self);
-        }
-    }
-
     let write = Message::Write(String::from("hello world"));
     let move_ = Message::Move { x: 100, y: 20 };
 
