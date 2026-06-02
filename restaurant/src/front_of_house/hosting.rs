dac2fc4b2fa5f34b64bc01a4a59c5189eeed6303
scailancrei <juanantonio.dev@hotmail.com>
2026-06-02 14:24:27 +0200

    crates and modules finished

---
+++ restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,9 @@
+pub mod hosting {
+    pub fn add_to_waitlist() {}
+    pub fn seat_at_table() {}
+    pub fn take_order() {}
+    pub fn serve_order() {}
+    pub fn take_payment() {}
+    pub fn thank_you() {}
+    pub fn goodbye() {}
+}
---
+++ restaurant/src/lib.rs
@@ -0,0 +1,17 @@
+mod back_of_house;
+mod front_of_house;
+
+pub use crate::back_of_house::breakfast::Breakfast;
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal.
+    meal.seasonal_fruit = String::from("blueberries");
+}
